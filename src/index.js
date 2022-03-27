da9ad2f3686de69fac3a49f0ae402bab08cfd7f4
ishi720 <ishizuka720@gmail.com>
2022-03-27 15:05:57 +0900

    ボタンクリックで棒グラフを再レンダーする

--- src/index.js
+++ src/index.js
@@ -5,47 +5,54 @@ function coinToss(){
     return coin[Math.floor(Math.random() * coin.length)];
 }
 
+function createData(trialCount,coinTossCount) {
+  //trialCountの数だけ実施
+  var coinTossRes = [];
+  for (var i=0;i<trialCount;i++) {
+      var omote = 0;
+      var ura = 0;
+
+      //コイントスをcoinTossCountの数だけ実施
+      for (var j=0;j<coinTossCount;j++) {
+          if (coinToss() == "表") {
+              omote++;
+          } else {
+              ura++;
+          }
+      }
+      //表になった回数を記録する
+      coinTossRes.push(omote);
+  }
+
+  // 重複している値をカウント
+  var count = {};
+  for (var i = 0;i<coinTossRes.length;i++) {
+    var elm = coinTossRes[i];
+    count[elm] = (count[elm] || 0) + 1;
+  }
+
+  // データ生成
+  var labelsArray = [];
+  var data = [];
+  for (var i=0;i<=coinTossCount;i++) {
+      labelsArray.push(i);
+      if (count[i]) {
+        data.push(count[i]);
+      } else {
+        data.push(0);
+      }
+  }
+  return [labelsArray,data]
+}
+
 var trialCount = 100000;
 var coinTossCount = 100;
 
-//trialCountの数だけ実施
-var coinTossRes = [];
-for (var i=0;i<trialCount;i++) {
-    var omote = 0;
-    var ura = 0;
+var [labelsArray,data] = createData(trialCount,coinTossCount);
 
-    //コイントスをcoinTossCountの数だけ実施
-    for (var j=0;j<coinTossCount;j++) {
-        if (coinToss() == "表") {
-            omote++;
-        } else {
-            ura++;
-        }
-    }
-    //表になった回数を記録する
-    coinTossRes.push(omote);
-}
-
-// 重複している値をカウント
-var count = {};
-for (var i = 0;i<coinTossRes.length;i++) {
-  var elm = coinTossRes[i];
-  count[elm] = (count[elm] || 0) + 1;
-}
-
-// データ生成
-var labelsArray = [];
-var data = [];
-for (var i=0;i<=coinTossCount;i++) {
-    labelsArray.push(i);
-    if (count[i]) {
-      data.push(count[i]);
-    } else {
-      data.push(0);
-    }
-}
 
 var ctx = document.getElementById('mychart');
+
 var myChart = new Chart(ctx, {
     type: 'bar',
     data: {
@@ -57,3 +64,11 @@ var myChart = new Chart(ctx, {
       }],
     }
 });
+
+function renderChart(trialCount,coinTossCount) {
+  var [labelsArray,dataArray] = createData(trialCount,coinTossCount);
+  myChart.data.labels = labelsArray;
+  myChart.data.datasets[0].data = dataArray;
+  myChart.update();
+}
+window.renderChart = renderChart;
