fa69d54f1605ca12ce7d2cae6adc588fb32f6fec
ishi720 <ishizuka720@gmail.com>
2023-11-27 00:20:10 +0900

    グラフの設定の修正

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import Chart from 'chart.js';
 import 'chota/dist/chota.min.css';
 
 /**
- * ベルヌーイ分布
+ * ベルヌーイ試行
  * @param {number} p 確率 (0.0 ~ 1.0)
  * @return {boolean} true:成功, false:失敗
  */
@@ -38,52 +38,67 @@ const createData = (trialCount,coinTossCount) => {
     // コイントスを実際に行い二項分布のデータを生成する
     for (let i=0; i<trialCount; i++) {
         count = binomialDistribution(coinTossCount, 0.5);
-        graphData[count] = graphData[count] + 1
+        graphData[count] = graphData[count] + 1;
     }
 
     // 結果を返却
-    return [labelsArray,graphData]
+    return [labelsArray,graphData];
 }
 
+/**
+ * グラフを更新する
+ */
+const updateChart = () => {
+    trialCount = Number(document.getElementsByClassName("trialCount")[0].value);
+    coinTossCount = Number(document.getElementsByClassName("coinTossCount")[0].value);
+    let [labelsArray,dataArray] = createData(trialCount,coinTossCount);
+    myChart.data.labels = labelsArray;
+    myChart.data.datasets[0].data = dataArray;
+    myChart.options.scales.xAxes[0].scaleLabel.labelString =  "コイントスを"+ coinTossCount +"回実施し表になった数回";
+    myChart.options.scales.yAxes[0].ticks.max = Math.round(trialCount / 10);
+    myChart.update();
+}
 
-
-
-let trialCount = 100000;
-let coinTossCount = 100;
+// メイン処理
+let trialCount = Number(document.getElementsByClassName("trialCount")[0].value);
+let coinTossCount = Number(document.getElementsByClassName("coinTossCount")[0].value);
 let [labelsArray,data] = createData(trialCount,coinTossCount);
 const ctx = document.getElementById('mychart');
-
-let myChart = new Chart(ctx, {
+const myChart = new Chart(ctx, {
     type: 'bar',
     data: {
         labels: labelsArray,
         datasets: [{
-          label: 'コインが〇回表になった回数',
-          data: data,
-          backgroundColor: "#ff9800",
-        }],
-        options: {
-            scales: {
-                xAxes: [{
-                    ticks: {
-                        min: 0
-                    }
-                }]
-            }
+            data: data,
+            backgroundColor: "#ff9800"
+        }]
+    },
+    options: {
+        legend: {
+            display: false,
+        },
+        scales: {
+            xAxes: [{
+                scaleLabel: {
+                    display: true,
+                    labelString: "コイントスを"+ coinTossCount +"回実施し表になった数回"
+                },
+            }],
+            yAxes: [{
+                scaleLabel: {
+                    display: true,
+                    labelString: "事象の回数を積み上げ"
+                },
+                ticks: {
+　　　　　　　　　　　max: Math.round(trialCount / 10),
+　　　　　　　　　}
+            }],
         }
     }
 });
 
-const renderChart = () => {
-    trialCount = Number(document.getElementsByClassName("trialCount")[0].value);
-    coinTossCount = Number(document.getElementsByClassName("coinTossCount")[0].value);
-    console.log(trialCount,coinTossCount);
-    let [labelsArray,dataArray] = createData(trialCount,coinTossCount);
-    myChart.data.labels = labelsArray;
-    myChart.data.datasets[0].data = dataArray;
-    myChart.update();
-}
-window.renderChart = renderChart;
+
+window.updateChart = updateChart;
 
 // //順列
 // const permutation = (n,r) => {
